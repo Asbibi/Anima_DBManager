54db854e2cfecca366ca576925df40d16a063de1
Asbibi <66073617+Asbibi@users.noreply.github.com>
2024-05-22 20:20:10 +0200

    UI -- Changed the background and foreground colors of disables attributes

--- Anima_DBManager/qattributedisplay.h
+++ Anima_DBManager/qattributedisplay.h
@@ -10,10 +10,14 @@ private:
     static QColor defaultColor;
     static QColor nullColor;
     static QColor invalidColor;
+    static QColor disabledColor;
+    static QColor defaultForegroundColor;
+    static QColor disabledForegroundColor;
     static QVariant noneCheckData;
 
     AttributeTypeHelper::Type myCurrentType = AttributeTypeHelper::Type::Invalid;
-    QString myCurrentValue;    
+    QString myCurrentValue;
+    bool myIsActiveAttribute;
 
     bool AskUpdateContent(const Attribute* _attribute);
     void UpdateContent(const Attribute* _attribute);
