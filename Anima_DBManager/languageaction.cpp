90c3f225b34157ecc045ec0a311cf0e862e6eaaa
Asbibi <66073617+Asbibi@users.noreply.github.com>
2026-07-15 09:52:25 +0200

    Refacto -- Renamed LanguageAction.myLanguageFinalValue into LanguageAction.myLanguageValue

--- Anima_DBManager/languageaction.cpp
+++ Anima_DBManager/languageaction.cpp
@@ -2,17 +2,17 @@
 
 LanguageAction::LanguageAction(const Language& _languageValue) :
     myType{LanguageActionType::PRISTINE},
-    myLanguageFinalValue{_languageValue}
+    myLanguageValue{_languageValue}
 {}
 
 
 LanguageAction::LanguageAction(const LanguageAction& _other) :
     myType{_other.myType},
-    myLanguageFinalValue{_other.myLanguageFinalValue}
+    myLanguageValue{_other.myLanguageValue}
 {}
 
 void LanguageAction::operator=(const LanguageAction& _other)
 {
     myType = _other.myType;
-    myLanguageFinalValue = _other.myLanguageFinalValue;
+    myLanguageValue = _other.myLanguageValue;
 }
